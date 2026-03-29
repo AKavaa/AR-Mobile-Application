6f33a1270ce2508bcf2e71dfe20f951efd622a4f
GTafouna <tafouna.george@gmail.com>
2026-03-29 14:37:00 +0300

    feat: Made additions so that when the user presses a room a map appears guiding them

--- Assets/Scripts/RoomListUI.cs
+++ Assets/Scripts/RoomListUI.cs
@@ -1,10 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomListUI : MonoBehaviour
 {
-    public void OnSelectRoom(string roomName)
+    [SerializeField] private GameObject mapImageObject;
+    [SerializeField] private Image mapImage;
+
+    [SerializeField] private Sprite receptionMap;
+    [SerializeField] private Sprite libraryMap;
+    [SerializeField] private Sprite studyRoomMap;
+    [SerializeField] private Sprite bathroomMap;
+    [SerializeField] private Sprite elevatorMap;
+
+    public void ShowReception()  { ShowMap(receptionMap); }
+    public void ShowLibrary()    { ShowMap(libraryMap); }
+    public void ShowStudyRoom()  { ShowMap(studyRoomMap); }
+    public void ShowBathroom()   { ShowMap(bathroomMap); }
+    public void ShowElevator()   { ShowMap(elevatorMap); }
+
+    public void CloseMap()
     {
-        Debug.Log("Selected room: " + roomName);
-        // change map image here based on roomName
+        mapImageObject.SetActive(false);
+        gameObject.SetActive(true);
+    }
+
+    private void ShowMap(Sprite map)
+    {
+        mapImage.sprite = map;
+        mapImageObject.SetActive(true);
+        gameObject.SetActive(false);
     }
 }
